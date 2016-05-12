8b55453d48346009fdbcc423d3fc78e03b9e4daf
Zeynep Demiragli <zeynep.demiragli@gmail.com>
2016-05-12 09:57:40 +0200

    met config

---
+++ Nero/test/testNero_met.py
@@ -0,0 +1,281 @@
+import FWCore.ParameterSet.Config as cms
+import FWCore.ParameterSet.VarParsing as VarParsing
+import re
+import os
+
+process = cms.Process("nero")
+
+options = VarParsing.VarParsing ('analysis')
+options.register('isData',
+        False,
+        VarParsing.VarParsing.multiplicity.singleton,
+        VarParsing.VarParsing.varType.bool,
+        "True if running on Data, False if running on MC")
+
+options.register('isGrid', False, VarParsing.VarParsing.multiplicity.singleton,VarParsing.VarParsing.varType.bool,"Set it to true if running on Grid")
+options.register('nerohead', "XXX", VarParsing.VarParsing.multiplicity.singleton,VarParsing.VarParsing.varType.string,"Set to the head of the repository. use check_output 'git rev-parse HEAD' in the crab py file. active only if isGrid.")
+options.register("nerotag", "YYY", VarParsing.VarParsing.multiplicity.singleton,VarParsing.VarParsing.varType.string,"Set to the tag of the repository. use check_output 'git rev-parse HEAD' in the crab py file. active only if isGrid.")
+options.register('isParticleGun', False, VarParsing.VarParsing.multiplicity.singleton,VarParsing.VarParsing.varType.bool,"Set it to true if MonteCarlo is ParticleGun")
+options.register('is25ns', True, VarParsing.VarParsing.multiplicity.singleton,VarParsing.VarParsing.varType.bool,"Set it to true to run on 25ns data/MC")
+options.register('is50ns', False, VarParsing.VarParsing.multiplicity.singleton,VarParsing.VarParsing.varType.bool,"Set it to true to run on 50ns data/MC")
+
+options.parseArguments()
+isData = options.isData
+
+if options.is25ns and options.is50ns : 
+    raise('cannot run both on 25 and 50ns. Pick up one')
+if not options.is25ns and not options.is50ns:
+    raise('cannot run nor 25ns nor 50ns configuration. Pick up one.')
+
+if options.is25ns:
+    print "-> Loading 25ns configuration"
+if options.is50ns:
+    print "-> Loading 50ns configuration"
+if options.isData:
+    print "-> Loading DATA configuration"
+else:
+    print "-> Loading MC configuration"
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+# If you run over many samples and you save the log, remember to reduce
+# the size of the output by prescaling the report of the event number
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+
+#process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
+
+if isData:
+        
+   fileList = [
+       #'/store/data/Run2015D/MET/MINIAOD/16Dec2015-v1/50000/00EA1DB2-90AA-E511-AEEE-0025905C2CE6.root'
+       '/store/data/Run2015D/DoubleMuon/MINIAOD/16Dec2015-v1/10000/000913F7-E9A7-E511-A286-003048FFD79C.root'
+       ]
+else:
+   fileList = [
+       'file://eos/cms/store/mc/RunIIFall15MiniAODv2/TTbarDMJets_pseudoscalar_Mchi-1_Mphi-100_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/20000/0A4E9031-7CB9-E511-8ABE-02163E00EA21.root'       
+        #       '/store/mc/RunIIFall15MiniAODv2/TT_TuneCUETP8M1_13TeV-powheg-pythia8/MINIAODSIM/PU25nsData2015v1_76X_mcRun2_asymptotic_v12_ext3-v1/00000/02837459-03C2-E511-8EA2-002590A887AC.root'
+       ]
+### do not remove the line below!
+###FILELIST###
+
+process.source = cms.Source("PoolSource",
+	skipEvents = cms.untracked.uint32(0),
+    	fileNames = cms.untracked.vstring(fileList)
+        )
+
+# ---- define the output file -------------------------------------------
+process.TFileService = cms.Service("TFileService",
+        closeFileFast = cms.untracked.bool(True),
+        fileName = cms.string("NeroNtuples.root"),
+        )
+
+##----------------GLOBAL TAG ---------------------------
+# used by photon id and jets
+process.load("Configuration.Geometry.GeometryIdeal_cff") 
+process.load('Configuration.StandardSequences.Services_cff')
+process.load("Configuration.StandardSequences.MagneticField_cff")
+
+#mc https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideFrontierConditions#Global_Tags_for_Run2_MC_Producti
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+if (isData):
+    if options.is25ns:
+        process.GlobalTag.globaltag = '76X_dataRun2_16Dec2015_v0'
+    if options.is50ns:
+        process.GlobalTag.globaltag = '74X_dataRun2_Prompt_v1'
+        print "FIX GLOBAL TAG"
+else:
+    #process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+    if options.is25ns:
+        process.GlobalTag.globaltag = '76X_mcRun2_asymptotic_RunIIFall15DR76_v1'
+    if options.is50ns:
+        process.GlobalTag.globaltag = 'MCRUN2_74_V9A::All' ## FIXME
+        print "FIX GLOBAL TAG"
+
+### LOAD DATABASE
+from CondCore.DBCommon.CondDBSetup_cfi import *
+
+######## LUMI MASK
+if isData and not options.isGrid : ## dont load the lumiMaks, will be called by crab
+    #pass
+    import FWCore.PythonUtilities.LumiList as LumiList
+    ## SILVER
+    #process.source.lumisToProcess = LumiList.LumiList(filename='/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON_Silver.txt').getVLuminosityBlockRange()
+    process.source.lumisToProcess = LumiList.LumiList(filename='/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON_Silver_v2.txt').getVLuminosityBlockRange()
+
+
+## SKIM INFO
+process.load('NeroProducer.Skim.infoProducerSequence_cff')
+process.load('NeroProducer.Nero.Nero_cfi')
+#process.load('NeroProducer.Nero.NeroMonotop_cfi')
+#process.load('NeroProducer.Nero.NeroMonojet_cfi')
+#process.load('NeroProducer.Nero.NeroChargedHiggs_cfi')
+
+#-----------------------ELECTRON ID-------------------------------
+from NeroProducer.Nero.egammavid_cfi import *
+
+initEGammaVID(process,options)
+
+### ##ISO
+process.load("RecoEgamma/PhotonIdentification/PhotonIDValueMapProducer_cfi")
+process.load("RecoEgamma/ElectronIdentification/ElectronIDValueMapProducer_cfi")
+
+#------------------- JER -----------------
+
+#era = 'Summer15_25nsV6'
+era = 'Fall15_25nsV2'
+
+if options.isData:
+    jerString = cms.string('sqlite:jer/'+era+'_DATA.db')
+    jerTags   = era+"_DATA"
+else:
+    jerString = cms.string('sqlite:jer/'+era+'_MC.db')
+    jerTags   = era+"_MC"
+
+process.jer = cms.ESSource("PoolDBESSource",
+        CondDBSetup,
+        toGet = cms.VPSet(
+            # Resolution
+            cms.PSet(
+                record = cms.string('JetResolutionRcd'),
+                tag    = cms.string('JR_'+jerTags+'_PtResolution_AK4PFchs'),
+                label  = cms.untracked.string('AK4PFchs_pt')
+                ),
+            # Phi
+            cms.PSet(
+                record = cms.string('JetResolutionRcd'),
+                tag    = cms.string('JR_'+jerTags+'_PhiResolution_AK4PFchs'),
+                label  = cms.untracked.string('AK4PFchs_phi')
+                ),        
+            # Scale factors
+            cms.PSet(
+                record = cms.string('JetResolutionScaleFactorRcd'),
+                tag    = cms.string('JR_'+jerTags+'_SF_AK4PFchs'),
+                label  = cms.untracked.string('AK4PFchs')
+                ),
+            ),
+        connect = jerString
+        )
+
+process.es_prefer_jer = cms.ESPrefer('PoolDBESSource', 'jer')
+
+
+################ end sqlite connection
+#### RECOMPUTE JEC From GT ###
+from PhysicsTools.PatAlgos.tools.jetTools import updateJetCollection
+
+jecLevels= ['L1FastJet',  'L2Relative', 'L3Absolute']
+if options.isData:
+        jecLevels =['L1FastJet',  'L2Relative', 'L3Absolute', 'L2L3Residual']
+
+updateJetCollection(
+   process,
+   jetSource = process.nero.jets,
+   labelName = 'UpdatedJEC',
+   jetCorrections = ('AK4PFchs', cms.vstring(jecLevels), 'None')  # Do not forget 'L2L3Residual' on data!
+)
+print "-> Updating the jets collection to run on to 'updatedPatJetsUpdatedJEC' with the new jec in the GT"
+process.nero.jets=cms.InputTag('updatedPatJetsUpdatedJEC')
+process.jecSequence = cms.Sequence( process.patJetCorrFactorsUpdatedJEC* process.updatedPatJetsUpdatedJEC )
+
+############ RECOMPUTE MET #######################
+from PhysicsTools.PatUtils.tools.runMETCorrectionsAndUncertainties import runMetCorAndUncFromMiniAOD
+
+runMetCorAndUncFromMiniAOD(process,
+            isData=isData,
+            )
+
+print "-> Updating the met collection to run on to 'slimmedMETs with nero' with the new jec in the GT for Type1"
+
+### redoPuppi=True
+### if redoPuppi:
+###   from PhysicsTools.PatAlgos.slimming.puppiForMET_cff import makePuppiesFromMiniAOD
+###   makePuppiesFromMiniAOD( process );
+### 
+###   runMetCorAndUncFromMiniAOD(process,
+###                              isData=runOnData,
+###                              pfCandColl=cms.InputTag("puppiForMET"),
+###                              recoMetFromPFCs=True,
+###                              reclusterJets=True,
+###                              jetFlavor="AK4PFPuppi",
+###                              postfix="Puppi"
+###                              )
+## no sequence ? , no change in input Tag ? 
+
+
+# ------------------------QG-----------------------------------------------
+# after jec, because need to be run on the corrected (latest) jet collection
+qgDatabaseVersion = '76X'
+
+# to use the database, see https://twiki.cern.ch/twiki/bin/view/CMS/QuarkGluonLikelihood
+#connect = cms.string('frontier://FrontierProd/CMS_COND_PAT_000'),
+#for type in ['AK4PFchs','AK4PFchs_antib']:
+#  QGPoolDBESSource.toGet.extend(cms.VPSet(cms.PSet(
+#    record = cms.string('QGLikelihoodRcd'),
+#    tag    = cms.string('QGLikelihoodObject_'+qgDatabaseVersion+'_'+type),
+#    label  = cms.untracked.string('QGL_'+type)
+#  )))
+
+process.QGPoolDBESSource = cms.ESSource("PoolDBESSource",
+      CondDBSetup,
+      toGet = cms.VPSet(
+        cms.PSet(
+            record = cms.string('QGLikelihoodRcd'),
+            tag    = cms.string('QGLikelihoodObject_'+qgDatabaseVersion+'_AK4PFchs'),
+            label  = cms.untracked.string('QGL_AK4PFchs')
+        ),
+      ),
+      connect = cms.string('sqlite:qg/QGL_'+qgDatabaseVersion+'.db')
+)
+process.es_prefer_qg = cms.ESPrefer('PoolDBESSource','QGPoolDBESSource')
+
+process.load('RecoJets.JetProducers.QGTagger_cfi')
+process.QGTagger.srcJets             = process.nero.jets   # Could be reco::PFJetCollection or pat::JetCollection (both AOD and miniAOD)               
+process.QGTagger.srcVertexCollection = process.nero.vertices
+process.QGTagger.useQualityCuts = cms.bool(False)
+
+###############################
+
+if options.isGrid:
+	process.nero.head=options.nerohead ##'git rev-parse HEAD'
+	process.nero.tag=options.nerotag ## git describe --tags
+
+if options.isParticleGun:
+	process.nero.particleGun = cms.untracked.bool(True)
+	## this option is for the embedding informations
+	process.nero.extendEvent = cms.untracked.bool(False)
+
+process.load('NeroProducer.Skim.MonoJetFilterSequence_cff')
+
+##DEBUG
+##print "Process=",process, process.__dict__.keys()
+#------------------------------------------------------
+process.p = cms.Path(
+                process.infoProducerSequence *
+                process.egmGsfElectronIDSequence *
+                process.egmPhotonIDSequence *
+                process.photonIDValueMapProducer * ## ISO MAP FOR PHOTONS
+                process.electronIDValueMapProducer *  ## ISO MAP FOR PHOTONS
+                #process.jecSequence *
+                #process.QGTagger    * ## after jec, because it will produce the new jet collection
+                process.fullPatMetSequence ## no puppi
+                #process.nero
+                )
+
+## DEBUG -- dump the event content with all the value maps ..
+process.output = cms.OutputModule(
+                "PoolOutputModule",
+                      fileName = cms.untracked.string('output.root'),
+                      )
+process.output_step = cms.EndPath(process.output)
+
+process.schedule = cms.Schedule(
+		process.p,
+		process.output_step)
+
+# Local Variables:
+# mode:c++
+# indent-tabs-mode:nil
+# tab-width:4
+# c-basic-offset:4
+# End:
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
