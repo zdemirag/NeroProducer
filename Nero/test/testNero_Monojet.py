2c96c4807c912d04416f7ef38bab8e8e34e15aab
Zeynep Demiragli <zeynep.demiragli@gmail.com>
2016-10-03 11:40:27 +0200

    renaming and minor config update

--- Nero/test/testNero_Monojet.py
+++ Nero/test/testNero_Monojet.py
@@ -86,7 +86,7 @@ from CondCore.DBCommon.CondDBSetup_cfi import *
 
 ## SKIM INFO
 process.load('NeroProducer.Skim.infoProducerSequence_cff')
-process.load('NeroProducer.Nero.Nero_cfi_monojetZeynep')
+process.load('NeroProducer.Nero.Nero_Monojet_cfi')
 
 #-----------------------ELECTRON ID-------------------------------
 from NeroProducer.Nero.egammavid_cfi import *
